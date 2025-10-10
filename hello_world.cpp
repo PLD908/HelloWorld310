b84052f1746c4337bf13a2899204c3a332fab9f0
Rachel Ukpabi <ukpabirachel456@gmail.com>
2025-10-10 08:04:21 -0700

    Initial Hello World commit

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World from Rachel!" << endl;
+    return 0;
+}
+
+// this is a simple c++ program that prints "hello, world!" to the console
